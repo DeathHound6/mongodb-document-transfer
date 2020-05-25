0ffa0970ffb70eed2cba2c866bed1d9b4ad29d8c
Glitch (mongodb-transfer-collections) <none>
2020-05-25 07:18:07 +0000

    Fix Topology issue

--- server.js
+++ server.js
@@ -13,24 +13,21 @@ if (!cs1 || !cs2) {
   process.exit();
 }
 
-const conn1 = new mongo(cs1, options);
-const conn2 = new mongo(cs2, options);
-
 try {
-  conn1.connect(err => {
+  mongo.connect(cs1, options, async (err, conn1) => {
     if (err) return console.error(err);
-    conn2.connect(async err => {
+    mongo.connect(cs2, options, async (err, conn2) => {
       const coll1 = conn1.db("").collection(""); // change both strings for your own needs
-      const coll2 = conn2.db("").collection(""); // change both strings for your own needs
+      const coll2 = conn1.db("").collection(""); // change both strings for your own needs
 
       const docs1 = await coll1.find({}).toArray();
       const docs2 = await coll2.find({}).toArray();
       const docs = [];
-      docs1.forEach(e=>{
-        if(!docs2.includes(e)) docs.push(e); // ensures it only adds documents that aren't already in it
-      })
-      setTimeout(()=>{
-        coll2.insert(docs); // `.insert()` can take an Object or an Array of Objects
+      docs1.forEach(e => {
+        if (!docs2.includes(e)) docs.push(e); // ensures it only adds documents that aren't already in it
+      });
+      setTimeout(() => {
+        coll2.insertMany(docs);
       }, 5000);
 
       console.log("Documents have been successfully transferred");
